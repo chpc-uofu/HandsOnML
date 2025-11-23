387b78e37ce32707dcd59636f841695aff8e783d
Wim R.M. Cardoen <wcardoen@gmail.com>
2025-11-23 14:17:42 -0700

    add checks

---
+++ notebooks/check_gpu.py
@@ -0,0 +1,12 @@
+#
+import torch
+
+def check_gpu_support():
+    if torch.cuda.is_available():
+        print(f"\nGPU device(s) found!")
+        print(f"  #GPUs: {torch.cuda.device_count()}")
+        print(f"  Current GPU: {torch.cuda.get_device_name(0)}")
+        print(f"  CUDA version: {torch.version.cuda}")
+    else:
+        print("\nNO GPU device detected.")
+check_gpu_support()
