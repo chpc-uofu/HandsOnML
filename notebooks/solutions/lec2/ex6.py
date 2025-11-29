b6f66de910e931e4cb64f42cedf624aeaed1abbd
Wim R.M. Cardoen <wcardoen@gmail.com>
2025-11-28 17:19:16 -0700

    update lecture2

--- notebooks/solutions/lec2/ex6.py
+++ notebooks/solutions/lec2/ex6.py
@@ -5,7 +5,7 @@ ind = int(np.argmax(lstAcc))
 d = lstResH2Layers[ind]
 label = d['model'].name
 obj = d['model']
-loss_val, acc_val = obj.evaluate(X_test_s, y_test)
+loss_test, acc_test = obj.evaluate(X_test_s, y_test)
 print(f"Model:{label}")
-print(f"  Loss: {loss_val}")
-print(f"  Acc:  {acc_val}")
+print(f"  Loss: {loss_test}")
+print(f"  Acc:  {acc_test}")
